c781d43bf3f6a95ba27bcfa041806b30ff496850
compylme <adthompson10@gmail.com>
2026-08-03 00:31:38 +0100

    dashboard types

---
+++ web/lib/analytics/types.ts
@@ -0,0 +1,46 @@
+export type CrawlerEventRow = {
+  received_at: string;
+  bot_name: string | null;
+  platform: string | null;
+  bot_type: string;
+  page_path: string | null;
+  page_url: string;
+};
+
+export type OverviewStats = {
+  totalCrawls: number;
+  uniquePlatforms: number;
+  uniquePages: number;
+  uniqueBots: number;
+  lastSeenAt: string | null;
+};
+
+export type PlatformBreakdownItem = {
+  platform: string;
+  botName: string;
+  botType: string;
+  crawlCount: number;
+  lastSeenAt: string | null;
+};
+
+export type TopPageItem = {
+  pagePath: string;
+  pageUrl: string;
+  crawlCount: number;
+  crawlers: string[];
+};
+
+export type ActivityLogEvent = {
+  receivedAt: string;
+  botName: string;
+  platform: string;
+  pagePath: string;
+  pageUrl: string;
+};
+
+export type SiteAnalytics = {
+  overview: OverviewStats;
+  platforms: PlatformBreakdownItem[];
+  topPages: TopPageItem[];
+  activityLog: ActivityLogEvent[];
+};
